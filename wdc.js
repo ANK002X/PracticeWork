17b70889557e2f2d434370cf03575eac431efea2
Aditya K <160767869+ANK002X@users.noreply.github.com>
2025-03-13 16:06:10 -0400

    Update wdc.js
    Added debugging code

--- wdc.js
+++ wdc.js
@@ -1,9 +1,12 @@
 (function () {
   var myConnector = tableau.makeConnector();
+
   myConnector.getData = function (request, doneCallback) {
+    console.log("Fetching data...");  // Log to see if this function is triggered
     var apiUrl = 'https://data.cityofchicago.org/resource/x2n5-8w5q.csv?$order=id&$limit=10000&$offset=0';
 
     $.get(apiUrl, function (data) {
+      console.log("Data fetched successfully:", data);  // Log the fetched data
       var tableData = [];
       data.forEach(function (row) {
         tableData.push({
@@ -14,6 +17,8 @@
       });
       request.respond(tableData);
       doneCallback();
+    }).fail(function() {
+      console.log("Error fetching data");
     });
   };
 
