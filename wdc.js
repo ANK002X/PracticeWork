aafabae225035275252b64f60647d7e53ad73105
Aditya K <160767869+ANK002X@users.noreply.github.com>
2025-03-13 16:07:40 -0400

    Update wdc.js
    matched it with html file

--- wdc.js
+++ wdc.js
@@ -2,11 +2,11 @@
   var myConnector = tableau.makeConnector();
 
   myConnector.getData = function (request, doneCallback) {
-    console.log("Fetching data...");  // Log to see if this function is triggered
+    console.log("Fetching data...");
     var apiUrl = 'https://data.cityofchicago.org/resource/x2n5-8w5q.csv?$order=id&$limit=10000&$offset=0';
 
     $.get(apiUrl, function (data) {
-      console.log("Data fetched successfully:", data);  // Log the fetched data
+      console.log("Data fetched successfully:", data);
       var tableData = [];
       data.forEach(function (row) {
         tableData.push({
